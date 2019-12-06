09cd73fb6d7e343421dd51195c24d8756bbd4c87
Siarhei Beliakou <sbeliakou@gmail.com>
2019-12-06 09:17:53 +0300

    for win

---
+++ lab.ps1
@@ -0,0 +1,26 @@
+Set-Location $PSScriptRoot
+
+{git reset --hard HEAD} | Out-Null
+
+{git pull -f} | Out-Null
+
+$standName = $args[1]
+
+function Get-Status {
+    [string[]](Get-Content trainings.yaml | Select-String -Pattern 'name:') | Foreach {$_.Trim().Split(' ')[2]}
+}
+
+Function Docker-Start {
+    docker-compose -f $(Join-Path -Path $PSScriptRoot -ChildPath "envs\$standName.yaml") up -d
+}
+
+Function Docker-Stop {
+    docker-compose -f $(Join-Path -Path $PSScriptRoot -ChildPath "envs\$standName.yaml") down --volumes
+}
+
+switch ($args[0]){
+    'status' {Get-Status}
+    'start' {Docker-Start}
+    'restart' {Docker-Stop;Docker-Start}
+    'stop' {Docker-Stop}
+}
