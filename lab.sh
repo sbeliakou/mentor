3dfe18bc9e472e003e7e943882002b9a9f6fd4e6
Siarhei Beliakou <sbeliakou@gmail.com>
2019-12-12 12:39:38 +0300

    upd

--- lab.sh
+++ lab.sh
@@ -7,13 +7,13 @@ git pull -f 1>/dev/null 2>&1
 standName=${2}
 
 function getRunning(){
-  docker ps -qa --filter label=lab
+  docker ps -qa --filter label=lab | head -1
 }
 
 function status() {
   if [[ -n "$(getRunning)" ]]; then
     echo Running Trainings:
-    TRAINING=$(docker inspect $(getRunning | head -1) -f '{{.Config.Labels.training}}')
+    TRAINING=$(docker inspect $(getRunning) -f '{{.Config.Labels.training}}')
     docker ps -a --filter label=lab \
       --format "table {{.ID}}|{{.Names}}|{{.Status}}|TRAINING|URL" | 
     sed 's/^\([0-9a-f].*\)URL/\1http:\/\/localhost:8081/;s/^\([0-9a-f].*\)TRAINING/\1'${TRAINING}'/' |
@@ -33,7 +33,7 @@ function start() {
 
 function stop() {
   if [[ -n "$(getRunning)" ]]; then
-    TRAINING=$(docker inspect $(getRunning | head -1) -f '{{.Config.Labels.training}}' | sed 's/<no value>//')
+    TRAINING=$(docker inspect $(getRunning) -f '{{.Config.Labels.training}}' | sed 's/<no value>//')
     echo ${TRAINING}
     if [ -n "${TRAINING}" ]; then
       docker-compose -f envs/${TRAINING}.yaml down --volumes
